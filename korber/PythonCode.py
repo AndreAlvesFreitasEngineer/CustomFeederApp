aa3a02dd05b12e98cb9843da673d479dc5091754
drealves <48499563+drealves@users.noreply.github.com>
2024-03-31 21:42:06 +0100

    last
    11

--- korber/PythonCode.py
+++ korber/PythonCode.py
@@ -2,29 +2,30 @@ import json
 import random
 from datetime import datetime, timedelta
 from geopy.distance import geodesic
+import time
 
 PLACES = {
-"New York City":(40.7128,-74.0060),
-"Times Square":(40.7580,-73.9855),
-"Empire State Building":(40.748817,-73.985428),
-"Central Park":(40.7851,-73.9683),
-"Brooklyn Bridge":(40.7061,-73.9969),
-"Statue of Liberty":(40.6892,-74.0445),
-"Grand Central Terminal":(40.7527,-73.9772),
-"One World Trade Center":(40.7126,-74.0133),
-"Rockefeller Center":(40.7587,-73.9787),
-"The Metropolitan Museum of Art":(40.7794,-73.9632),
-"Broadway":(40.7590,-73.9845),
-"Madison Square Garden":(40.7505,-73.9934),
-"Brooklyn Botanic Garden":(40.6694,-73.9626),
-"Coney Island":(40.5749,-73.9851),
-"High Line":(40.7479,-74.0048),
-"Yankee Stadium":(40.8296,-73.9262),
-"The Cloisters":(40.8648,-73.9316),
-"Wall Street":(40.7074,-74.0119),
-"Central Park Zoo":(40.7678,-73.9718),
-"Fifth Avenue":(40.7812,-73.9665),
-# Add more real places as needed
+    "New York City":(40.7128,-74.0060),
+    "Times Square":(40.7580,-73.9855),
+    "Empire State Building":(40.748817,-73.985428),
+    "Central Park":(40.7851,-73.9683),
+    "Brooklyn Bridge":(40.7061,-73.9969),
+    "Statue of Liberty":(40.6892,-74.0445),
+    "Grand Central Terminal":(40.7527,-73.9772),
+    "One World Trade Center":(40.7126,-74.0133),
+    "Rockefeller Center":(40.7587,-73.9787),
+    "The Metropolitan Museum of Art":(40.7794,-73.9632),
+    "Broadway":(40.7590,-73.9845),
+    "Madison Square Garden":(40.7505,-73.9934),
+    "Brooklyn Botanic Garden":(40.6694,-73.9626),
+    "Coney Island":(40.5749,-73.9851),
+    "High Line":(40.7479,-74.0048),
+    "Yankee Stadium":(40.8296,-73.9262),
+    "The Cloisters":(40.8648,-73.9316),
+    "Wall Street":(40.7074,-74.0119),
+    "Central Park Zoo":(40.7678,-73.9718),
+    "Fifth Avenue":(40.7812,-73.9665),
+    # Add more real places as needed
 }
 
 RATE_PER_KM=2.5
@@ -64,7 +65,12 @@ def generate_entry():
         "price": price
     }
 
-entries = [generate_entry() for _ in range(10000)]
+# Execute a cada 20 minutos (3 vezes por hora)
+while True:
+    entries = [generate_entry() for _ in range(10000)]
 
-with open("taxi_records.json", "w") as f:
-    json.dump(entries, f, indent=2)
+    with open("taxi_records.json", "w") as f:
+        json.dump(entries, f, indent=2)
+    
+    # Aguarda 20 minutos (1200 segundos) antes de gerar os próximos registros
+    time.sleep(1200)
--- korber/src/main/java/com/data/korber/schedule/PythonScriptSchedulerConfig.java
+++ korber/src/main/java/com/data/korber/schedule/PythonScriptSchedulerConfig.java
@@ -1,5 +1,8 @@
 package com.data.korber.schedule;
+import com.data.korber.messaging.JsonFileReaderService;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
@@ -10,6 +13,8 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class PythonScriptSchedulerConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonFileReaderService.class);
+
     @PostConstruct
     public void schedulePythonScriptExecution() {
         // Create a scheduled executor service
@@ -18,6 +23,7 @@ public class PythonScriptSchedulerConfig {
         // Define the task to run Python script
         Runnable pythonScriptTask = () -> {
             try {
+                LOGGER.info("Start Generate File");
                 // Execute Python script
                 Process process = Runtime.getRuntime().exec("python PythonCode.py");
                 // You can add more code to handle the process if needed
@@ -26,7 +32,7 @@ public class PythonScriptSchedulerConfig {
             }
         };
 
-        // Schedule the task to run every 20 minutes (3 times an hour)
-        executorService.scheduleAtFixedRate(pythonScriptTask, 0, 20, TimeUnit.MINUTES);
+        // Schedule the task to run every 10 minutes (6 times an hour)
+        executorService.scheduleAtFixedRate(pythonScriptTask, 0, 10, TimeUnit.MINUTES);
     }
 }
